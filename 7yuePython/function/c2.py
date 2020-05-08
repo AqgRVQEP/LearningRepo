9ba0634ebf8e8de2e14bd841c3d8010a40ca4f93
kylechou <kylechou94@gmail.com>
2020-05-08 11:55:11 +0800

    update

---
+++ 7yuePython/function/c2.py
@@ -0,0 +1,21 @@
+
+# def funcname(parameter_list):
+#     pass
+
+# 1. 参数列表可以没有
+# 2. retuen value None
+
+# 1. 实现两个数字的相加
+# 2. 打印输入的参数
+
+def add(x, y):
+    result = x + y
+    return result
+
+def print_code(code):
+    print(code)
+
+a = add(1, 2)
+b = print_code('Python')
+
+print(a, b)
