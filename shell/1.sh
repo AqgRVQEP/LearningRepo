6751aaa0653342488646455d64703a59098092b9
kylechou <kylechou94@gmail.com>
2020-05-09 12:01:29 +0800

    update

---
+++ shell/1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -n "Enter your name: "
+read name
+echo "hello ${name}, welcome to my program."
+exit 0
---
+++ shell/2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# echo -n "Enter your name: "
+read -p "Enter your name: " name
+# read name
+echo "hello ${name}, welcome to my program."
+exit 0
