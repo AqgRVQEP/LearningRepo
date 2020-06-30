84f18038ce89923de3ba66b5d9376a0a26908d66
Kyle Chou <kylechou94@gmail.com>
2020-06-30 18:11:53 +0800

    update

--- Mastering-Python-Scripting-for-System-Administrators--master/Chapter02/timeit_example.py
+++ Mastering-Python-Scripting-for-System-Administrators--master/Chapter02/timeit_example.py
@@ -5,7 +5,16 @@ def timeit_example():
 	list1 = []
 	for x in range(50):
 		list1.append(sqrt(x))
-''' 
+'''
 # timeit statement
-print(timeit.timeit(setup = prg_setup, stmt = prg_code, number = 10000))
+print(timeit.timeit(setup = prg_setup, stmt = prg_code, number = 100000))
 
+# from math import sqrt
+
+# def timeit_example():
+# 	list1 = []
+# 	for x in range(50):
+# 		list1.append(sqrt(x))
+# 	print(list1)
+
+# timeit_example()
